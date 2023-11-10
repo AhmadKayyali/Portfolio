9e08768b6df264ec03d957be5d16dd99bd129511
AhmadKayyali <ahmadkayy@gmail.com>
2023-11-10 19:13:36 +1100

    home page complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.scss";
+import React, { useEffect } from "react";
+import Typed from 'typed.js';
 
 function App() {
+  const el = React.useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['Hi there, welcome to my website! ', 'My name is Ahmad, I\'m a Software Engineering student at RMIT University.', 'Have a look through my page and feel free to contact me for any opportunities!'],
+      typeSpeed: 50,
+      startDelay: 1000,
+      backDelay: 1000,
+      backSpeed: 50,
+      showCursor: false,
+      loop: true
+    });
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html lang="en">
+      <body>
+
+        <div class="home-container">
+
+        <div class="text-container">
+        <span ref={el} />
+        </div>
+
+        <div className="gradient-bg">
+          <svg xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <filter id="goo">
+                <feGaussianBlur
+                  in="SourceGraphic"
+                  stdDeviation="10"
+                  result="blur"
+                />
+                <feColorMatrix
+                  in="blur"
+                  mode="matrix"
+                  values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                  result="goo"
+                />
+                <feBlend in="SourceGraphic" in2="goo" />
+              </filter>
+            </defs>
+          </svg>
+          <div className="gradients-container">
+            <div className="g1"></div>
+            <div className="g2"></div>
+            <div className="g3"></div>
+            <div className="g4"></div>
+            <div className="g5"></div>
+          </div>
+        </div>
+      </div>
+      </body>
+    </html>
   );
 }
 
---
+++ my-app/src/Navbar.js
@@ -0,0 +1,21 @@
+// Navbar component
+
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar-container">
+        
+    <div className="inner-container">
+      <div className="logo">Home</div>
+      <ul className="option-buttons">
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
